509dd95f14177f83c84f929abb258dabe75df522
tuantq57 <tuantq.vnu@gmail.com>
2014-03-27 14:43:48 +0700

    [#67] add Tree class

---
+++ Classes/Tree.cpp
@@ -0,0 +1,9 @@
+//
+//  Tree.cpp
+//  landlord
+//
+//  Created by Tuan TQ on 3/27/14.
+//
+//
+
+#include "Tree.h"
---
+++ Classes/Tree.h
@@ -0,0 +1,14 @@
+//
+//  Tree.h
+//  landlord
+//
+//  Created by Tuan TQ on 3/27/14.
+//
+//
+
+#ifndef __landlord__Tree__
+#define __landlord__Tree__
+
+#include <iostream>
+
+#endif /* defined(__landlord__Tree__) */
