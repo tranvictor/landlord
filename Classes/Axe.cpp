df34656222e037cb4efedfdd95ce2b878efda4c7
tuantq57 <tuantq.vnu@gmail.com>
2014-03-27 14:44:16 +0700

    [#67] add Axe class

---
+++ Classes/Axe.cpp
@@ -0,0 +1,9 @@
+//
+//  Axe.cpp
+//  landlord
+//
+//  Created by Tuan TQ on 3/27/14.
+//
+//
+
+#include "Axe.h"
---
+++ Classes/Axe.h
@@ -0,0 +1,14 @@
+//
+//  Axe.h
+//  landlord
+//
+//  Created by Tuan TQ on 3/27/14.
+//
+//
+
+#ifndef __landlord__Axe__
+#define __landlord__Axe__
+
+#include <iostream>
+
+#endif /* defined(__landlord__Axe__) */
