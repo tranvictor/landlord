dc2c27dafa9c357ee8f2dd5614f1aea03fd05c34
tuantq57 <tuantq.vnu@gmail.com>
2014-03-29 21:13:56 +0700

    [#67] add stone class

---
+++ Classes/stone.cpp
@@ -0,0 +1,9 @@
+//
+//  stone.cpp
+//  landlord
+//
+//  Created by Tuan TQ on 3/29/14.
+//
+//
+
+#include "stone.h"
---
+++ Classes/stone.h
@@ -0,0 +1,23 @@
+//
+//  stone.h
+//  landlord
+//
+//  Created by Tuan TQ on 3/29/14.
+//
+//
+
+#ifndef __landlord__stone__
+#define __landlord__stone__
+
+#include <iostream>
+
+class stone
+{
+private:
+  
+public:
+  
+  
+};
+
+#endif /* defined(__landlord__stone__) */
