a068538f993f51aedbd21dc3ef85584518b13442
tuantq57 <tuantq.vnu@gmail.com>
2014-04-18 17:23:11 +0700

    [#72] commit to push github

--- proj.ios/landlord Tests/ChooseCharacterScene Tests.mm
+++ proj.ios/landlord Tests/ChooseCharacterScene Tests.mm
@@ -25,8 +25,6 @@ ChooseCharacterScene *character;
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
   character = ChooseCharacterScene::create();
-  cocos2d::CCDirector *pDirector = cocos2d::CCDirector::sharedDirector();
-  pDirector->runWithScene(character->scene());
 }
 
 - (void)tearDown
