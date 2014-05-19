0192a164b517ee326c68ad3b7fff6247499ce089
Giap Nguyen <nvangiap5894@gmail.com>
2014-05-19 15:16:29 +0700

    ignore proj.android

--- Classes/PlayScene.cpp
+++ Classes/PlayScene.cpp
@@ -236,6 +236,7 @@ void PlayScene::pauseButtonTouched(CCObject* pSender)
   CCLog("paused touched");
   // TODO
   unscheduleUpdate();
+  unschedule(schedule_selector(PlayScene::cooldown));
   CREATE_MENU_ITEM(PlayScene, WinScene, CCTransitionFade);
   CocosDenshion::SimpleAudioEngine::sharedEngine()->stopBackgroundMusic();
   sound::playSoundFx(SFX_CONGRATULATION);
@@ -554,7 +555,9 @@ void PlayScene::update(float pdT)
       GameManager::getPlayerScore(PLAYER_ONE) > GameManager::getPlayerScore(PLAYER_TWO) ? PLAYER_ONE : PLAYER_TWO;
     
     /// What about equal ?????
+    
     unscheduleUpdate();
+    unschedule(schedule_selector(PlayScene::cooldown));
     GameManager::setWinPlayer(winPlayer);
     CCScene* newScene = CCTransitionCrossFade::create(0.5, WinScene::scene());
     CCDirector::sharedDirector()->replaceScene(newScene);
@@ -939,7 +942,7 @@ void PlayScene::setupRemindLayer()
   this->addChild(mReminder, GR_FOREGROUND);
 }
 
-void PlayScene::onResume()
+void PlayScene::onResume(cocos2d::CCObject* pSender)
 {
   sound::playSoundFx(SFX_BUTTON_TOUCH);
   mReminder->setVisible(false);
